9e1ff1f4e7d2b5df4c75541630de5ad1408ec216
GoofyGoat10 <biker04@web-desk.eu>
2025-06-13 11:57:51 +0200

    alte dateien gelöscht

--- !Alt/client.py
+++
@@ -1,9 +0,0 @@
-#import socket
-
-#def send_message(message, target_ip, target_port=5001):
- #   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  #  sock.sendto(message.encode(), (target_ip, target_port))
-   # print(f"Nachricht gesendet an {target_ip}:{target_port}")
-    #sock.close()
-
-#Wird aktuell nicht mehr direkt genutzt, weil wir jetzt alles über Queues regeln. Aber sie zeigt gut, wie einfach UDP in Python funktioniert.“
