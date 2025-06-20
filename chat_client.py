29311728c8fa51cd9543c48f46bd45c5ecf82cd3
RobinSp5 <50964683+RobinSp5@users.noreply.github.com>
2025-06-20 12:33:59 +0200

    Kommentare client.py

--- chat_client.py
+++ chat_client.py
@@ -4,6 +4,7 @@ from typing import Dict, Any
 
 
 class ChatClient:
+    # Konstruktor der ChatClient-Klasse
     def __init__(self, config: Dict[str, Any], username: str):
         self.config = config
         self.username = username
@@ -12,16 +13,20 @@ class ChatClient:
     def send_text_message(self, target_ip: str, target_port: int, target_handle: str, message: str) -> bool:
         """Sendet eine SLCP-Textnachricht über TCP (max. 512 Bytes inklusive Header)"""
         try:
+            # Prüfen, ob die Nachricht leer ist
             slcp_line = f"MSG {target_handle} {message}\n"
             encoded = slcp_line.encode("utf-8")
 
+            # Prüfen, ob die Nachricht zu lang ist (maximal 512 Bytes)
             if len(encoded) > 512:
                 print(f"Nachricht zu lang ({len(encoded)} Bytes). Maximal erlaubt: 512 Bytes.")
                 return False
 
+            
             with socket.create_connection((target_ip, target_port), timeout=self.config['system']['socket_timeout']) as sock:
-                sock.sendall(encoded)
+                sock.sendall(encoded) # Sende die SLCP-Nachricht
             return True
+        #Error-Handling
         except Exception as e:
             print(f"Text Message Error: {e}")
             return False
@@ -30,12 +35,14 @@ class ChatClient:
     def send_image_message(self, target_ip: str, target_port: int, target_handle: str, image_path: str) -> bool:
         """Sendet eine SLCP-Bildnachricht über TCP"""
         try:
+            # Prüfen, ob der Pfad zu einem Bild existiert
             if not os.path.exists(image_path):
                 print(f"Bild nicht gefunden: {image_path}")
                 return False
 
-            file_size = os.path.getsize(image_path)
-            max_size = self.config['user']['max_image_size']
+            file_size = os.path.getsize(image_path) # Größe des Bildes in Bytes
+            max_size = self.config['user']['max_image_size'] # Maximale Bildgröße in Bytes auss config auslesen
+            # Prüfen, ob die Bildgröße das Limit überschreitet
             if file_size > max_size:
                 print(f"Bild zu groß: {file_size} bytes (max: {max_size})")
                 return False
@@ -45,11 +52,13 @@ class ChatClient:
             with open(image_path, "rb") as f:
                 image_data = f.read()
 
+            # Stellt eine TCP-Verbindung zum Zielnutzer her und überträgt zuerst den SLCP-Header, dann die Bilddaten.
             with socket.create_connection((target_ip, target_port), timeout=self.config['system']['socket_timeout']) as sock:
                 sock.sendall(slcp_header.encode("utf-8"))
                 sock.sendall(image_data)
 
             return True
+        #Error-Handling
         except Exception as e:
             print(f"Image Message Error: {e}")
             return False
